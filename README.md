ade012634080f743de3e4a991b4aa1a45cc29047
igolaizola <11333576+igolaizola@users.noreply.github.com>
2023-02-19 00:02:25 +0100

    Added rename script and readme usage

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # goobar
+
 Skeleton for golang projects
+
+## Usage
+
+```bash
+git clone https://github.com/igolaizola/goobar project
+cd project
+./rename.sh owner/project
+```
---
+++ rename.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Treat unset variables as an error
+set -u
+# Exit on error
+set -e 
+
+# Obtain repository owner and name from argument
+owner=$(echo "$1" | cut -d'/' -f1)
+name=$(echo "$1" | cut -d'/' -f2)
+
+# Check if owner or name are empty
+if [ -z "$owner" ] || [ -z "$name" ]; then
+    echo "Invalid repository name '$1'"
+    exit 1
+fi
+
+# Lowercase and uppercase
+name_low=$(echo "$name" | tr '[:upper:]' '[:lower:]')
+name_upp=$(echo "$name" | tr '[:lower:]' '[:upper:]')
+
+# Rename files and folders
+mv cmd/goobar "cmd/$name_low"
+mv goobar.go "$name_low.go"
+
+# Replace file contents
+find . -type f -exec sed -i "s/goobar/$name_low/g" {} \;
+find . -type f -exec sed -i "s/GOOBAR/$name_upp/g" {} \;
+find . -type f -exec sed -i "s/igolaizola/$owner/g" {} \;
+
+# Remove this script folder
+rm -rf rename.sh
+
+# Remove .git folder
+rm -rf .git
