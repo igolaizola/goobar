38cc04202106bc85416e5db530c2cdd033b0808b
igolaizola <11333576+igolaizola@users.noreply.github.com>
2023-06-12 15:19:46 +0200

    Added version command

--- cmd/goobar/main.go
+++ cmd/goobar/main.go
@@ -4,14 +4,22 @@ import (
 	"context"
 	"errors"
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
+	"runtime/debug"
+	"strings"
 
 	"github.com/igolaizola/goobar"
 	"github.com/peterbourgon/ff/v3"
 	"github.com/peterbourgon/ff/v3/ffcli"
 )
+
+// Build flags
+var Version = ""
+var Commit = ""
+var Date = ""
 
 func main() {
 	// Create signal based context
@@ -35,8 +43,37 @@ func newCommand() *ffcli.Command {
 			return flag.ErrHelp
 		},
 		Subcommands: []*ffcli.Command{
+			newVersionCommand(),
 			newServeCommand(),
 			newRunCommand(),
+		},
+	}
+}
+
+func newVersionCommand() *ffcli.Command {
+	return &ffcli.Command{
+		Name:       "version",
+		ShortUsage: "goobar version",
+		ShortHelp:  "print version",
+		Exec: func(ctx context.Context, args []string) error {
+			v := Version
+			if v == "" {
+				if buildInfo, ok := debug.ReadBuildInfo(); ok {
+					v = buildInfo.Main.Version
+				}
+			}
+			if v == "" {
+				v = "dev"
+			}
+			versionFields := []string{v}
+			if Commit != "" {
+				versionFields = append(versionFields, Commit)
+			}
+			if Date != "" {
+				versionFields = append(versionFields, Date)
+			}
+			fmt.Println(strings.Join(versionFields, " "))
+			return nil
 		},
 	}
 }
